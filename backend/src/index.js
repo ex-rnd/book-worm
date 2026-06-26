265e3e1caa88067f72be00f32c8c3c50628c9cbf
James Kilonzo <jkilonzo2023@gmail.com>
2026-06-26 10:52:38 +0300

    Init backend - Express API

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+// const express = require("express");
+
+import express from "express";
+
+const app = express();
+const PORT = process.env;
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
